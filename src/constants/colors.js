e142f415b76dccce2fb5ff19714acb2ad550cf31
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2020-08-16 19:27:15 +0100

    constants and svgs

---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+	primary: "#14233C",
+	secondary: "#FE4D56",
+	tertiary: "#2A90CC",
+	backgroundColor: "#F7F7F7",
+	textColor: "#14233C",
+	buttonTextColor: "#F7F7F7",
+	white: "#fff",
+};
+
+export default COLORS;
---
+++ src/constants/mediaQueries.js
@@ -0,0 +1,10 @@
+const mediaQueries = {
+  mobileOnly: `(max-width: 599px)`,
+  tabletPortraitUp: `(min-width: 600px)`,
+  tabletLandscapeUp: `(min-width: 900px)`,
+  tabletLandscapeDown: `(max-width: 900px)`,
+  desktopUp: `(min-width: 1025px)`,
+  bigDesktopUp: `(min-width: 1800px)`,
+};
+
+export default mediaQueries;
