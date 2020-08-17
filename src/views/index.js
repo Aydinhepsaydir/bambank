4492084f1f2d27594dbe32009d07b38db7df86f9
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2020-08-17 18:53:37 +0100

    added views to index

--- src/views/index.js
+++ src/views/index.js
@@ -4,3 +4,5 @@ export { default as SignInView } from "./SignInView";
 export { default as SignUpView } from "./SignUpView";
 export { default as PasswordForgetView } from "./PasswordForgetView";
 export { default as ConfirmAccountView } from "./ConfirmAccountView";
+export { default as MakeTransactionView } from "./MakeTransactionView";
+export { default as CheckBalanceView } from "./CheckBalanceView";
