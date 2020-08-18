a2fbbe382ef5dc0d4fc592fdb693dd870aea5d84
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2020-08-18 13:25:49 +0100

    added firebase class and context

---
+++ src/contexts/Firebase/index.js
@@ -0,0 +1,5 @@
+import FirebaseContext, { withFirebase } from "./context";
+import Firebase, { firebase } from "./firebase";
+
+export default Firebase;
+export { FirebaseContext, withFirebase, firebase };
